352d428e8d9336baeb482caf4880c3caf6583014
lichin-lin <vic20087cjimlin@gmail.com>
2018-07-04 22:54:35 +0800

    add: onion mode

--- src/App.js
+++ src/App.js
@@ -1,20 +1,113 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import _ from 'lodash'
+import InputRange from 'react-input-range'
+import 'react-input-range/lib/css/index.css'
+import './App.css'
 
-class App extends Component {
+const mode = [
+  `onion`,
+  `normal`,
+  `slide`
+]
+const StyledApp = styled.div`
+  text-align: center;
+  width: 100%;
+  background: transparent;
+  --bg: #000;
+  --button-bg: #4a4a4a;
+  --button-color: #97a0a6;
+
+  padding: 10px 0;
+  border-bottom: 1px rgba(185,194,200,0.25) solid;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  ${'' /* opacity: var(--onion-mode-opacity); */}
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const ModeButn = styled.div`
+  background: var(--button-bg);
+  color: var(--button-color);
+  font-size: 14px;
+  line-height: 16px;
+
+  border-radius: 2px;
+  padding: 4px 12px;
+  margin: 4px;
+
+  cursor: pointer;
+`
+
+const RangeWrapper = styled.div`
+  width: 320px !important;
+  .input-range__track {
+    background: var(--button-color);
+  }
+  .input-range__track--background {
+    background: var(--button-bg);
+  }
+  .input-range__slider {
+    background: var(--button-color);
+    border: none;
+  }
+  .input-range__label-container {
+    display: none;
+  }
+`
+export default class App extends Component {
+  state = {
+    mode: `normal`,
+    value: 1
+  }
+  handleClick = (id) => {
+    const ref = document.getElementsByClassName('detail-and-compare')[0]
+    ref.setAttribute('data-mode', mode[id])
+    this.setState({ mode:  mode[id]})
+
+    if (mode[id] === `normal`) {
+
+    } else {
+
+    }
+  }
+
+  handleSlide = (value) => {
+    this.setState({ value })
+    if (this.state.mode === `onion`) {
+      console.log(value)
+      document.body.style.setProperty('--onion-mode-opacity', value)
+    } else {
+
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <StyledApp>
+        <Wrapper>
+          {
+            _.map(mode, (m, id) =>
+              <ModeButn key={id} onClick={() => this.handleClick(id)}>{m}</ModeButn>
+            )
+          }
+        </Wrapper>
+        <RangeWrapper>
+          <InputRange
+            maxValue={1}
+            minValue={0}
+            step={0.01}
+            value={this.state.value}
+            onChange={(value) => this.handleSlide(value)} />
+        </RangeWrapper>
+      </StyledApp>
+    )
   }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { injectGlobal } from 'styled-components';
 import registerServiceWorker from './registerServiceWorker';
 
-const target = document.getElementsByClassName('header')[0];
+const target = document.getElementsByClassName('artboard-preview')[0];
+const ref = document.getElementsByClassName('detail-and-compare')[0];
 const app = document.createElement('div');
 app.id = 'root';
-if (target) target.prepend(app);
+if (target && ref) target.insertBefore(app, ref)
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
+
+
+// Global Panel style
+injectGlobal`
+  --onion-mode-opacity: 1;
+
+  .detail-and-compare {
+    &[data-mode='slide'],
+    &[data-mode='onion'] {
+      display: flex;
+      justify-content: space-between;
+
+      > .detail-screen,
+      > .compare-screen {
+        position: absolute;
+        top: 50px;
+        left: 0;
+        padding: 0;
+        margin: 0 !important;
+        width: 100% !important;
+      }
+
+      > a.revision-link {
+        position: relative !important;
+      }
+    }
+    &[data-mode='slide'] {
+
+    }
+
+    &[data-mode='onion'] {
+      .detail-screen img {
+        opacity: var(--onion-mode-opacity);
+      }
+      .compare-screen img {
+        opacity: calc(1 - var(--onion-mode-opacity));
+      }
+    }
+  }
+`
